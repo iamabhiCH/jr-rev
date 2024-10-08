739226494ed2ec3710f94f34266bb30f4aa8314b
Abhishek Yadav <abhishekay442080@gmail.com>
2024-10-08 21:31:19 +0530

    map print last object

---
+++ Mapping.js
@@ -0,0 +1,19 @@
+const user = [
+    {
+        name: "John",
+        age: 50,
+        city: "Seattle",
+    },
+    {
+        name: "Carl",
+        age: 30,
+        city: "NYC",
+    },
+    {
+        name: "Paul",
+        age: 34,
+        city: "Chicago",
+    },
+];
+
+console.log(user[user.length-1])
